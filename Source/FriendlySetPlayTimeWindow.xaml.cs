74daba8175309c0abbf43dbeeb71291a06af2fd9
CanRanBan <git@canranban.eu>
2025-05-03 19:15:39 +0200

    Add license header

--- Source/FriendlySetPlayTimeWindow.xaml.cs
+++ Source/FriendlySetPlayTimeWindow.xaml.cs
@@ -1,3 +1,6 @@
+// This file is part of Friendly Set Play Time. A Playnite extension to simplify the input of play time values.
+// Copyright CanRanBan, 2022-2025, Licensed under the EUPL-1.2 or later.
+
 using System;
 using System.Text.RegularExpressions;
 using System.Windows;
